b1201f1006a42e335e790d317da07fbc44f5e6a3
Ladu-Lumori <ladulumori2@gmail.com>
2024-11-23 03:39:56 +0300

    Commit Force

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,8 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        'dark-gradient': 'linear-gradient(to top, rgba(0,0,0,0.8), transparent)'
       },
     },
   },
