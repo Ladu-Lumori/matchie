e65eac52bd0863cfa0f512c977031fde81e16ab0
Ladu-Lumori <ladulumori2@gmail.com>
2024-11-25 12:52:01 +0300

    ImageUploadButton Fix

--- src/components/ImageUploadButton.tsx
+++ src/components/ImageUploadButton.tsx
@@ -21,7 +21,7 @@ export default function ImageUploadButton({
             options={{ maxFiles: 1 }}
             onSuccess={onUploadImage}
             signatureEndpoint="/api/sign-image"
-            uploadPreset="matchme-demo"
+            uploadPreset="match"
             className={`flex items-center gap-2 border-2 border-default text-default 
         rounded-lg py-2 px-4 hover:bg-default/10`}
         >
